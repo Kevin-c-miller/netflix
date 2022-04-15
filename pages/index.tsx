e7ca91913de7968514943b0c8e3cbe720b71b084
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-04-15 16:43:27 -0400

    added styled to the main div in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,10 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
-// import '../styles/globals.css'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Netflix | Home</title>
         <link rel="icon" href="/favicon.ico" />
