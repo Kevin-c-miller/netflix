e986e906a753dd0f66945be8368f4dd3650271b7
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-04-15 19:00:02 -0400

    added scrollbar hide and scrollbar styling

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,7 +1,7 @@
 import { ChevronLeftIcon } from '@heroicons/react/outline'
 import { ChevronRightIcon } from '@heroicons/react/outline'
-import React from 'react'
 import { Movie } from '../types'
+import Thumbnail from './Thumbnail'
 
 interface Props {
   title: string
@@ -11,9 +11,18 @@ interface Props {
 export default function Row({ title, movies }: Props) {
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
-      <h2>{title}</h2>
+      <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
+        {title}
+      </h2>
       <div className="group relative md:-ml-2">
         <ChevronLeftIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
+
+        <div className=" flex items-center space-x-0.5 overflow-x-hidden scrollbar-hide md:space-x-2.5 md:p-2">
+          {movies.map((movie) => (
+            <Thumbnail key={movie.id} movie={movie} />
+          ))}
+        </div>
+
         <ChevronRightIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
       </div>
     </div>
