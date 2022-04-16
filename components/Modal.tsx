0f7d1d1c30b4bf4b61a980168687c8a1c8ba4c8f
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-04-16 16:42:17 -0400

    finished modal and styling

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -17,7 +17,7 @@ export default function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState)
   const [movie, setMovie] = useRecoilState(movieState)
   const [trailer, setTrailer] = useState('')
-  const [genre, setGenre] = useState<Genre[]>()
+  const [genres, setGenres] = useState<Genre[]>()
   const [muted, setMuted] = useState(false)
 
   const handleClose = () => {
@@ -46,7 +46,7 @@ export default function Modal() {
         setTrailer(data.videos?.results[index]?.key)
       }
       if (data?.genres) {
-        setGenre(data.genres)
+        setGenres(data.genres)
       }
     }
     fetchMovie()
@@ -99,6 +99,40 @@ export default function Modal() {
                 <VolumeUpIcon className="h-6 w-6" />
               )}
             </button>
+          </div>
+        </div>
+
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">
+                {movie!.vote_average * 10}% match
+              </p>
+              <p className="font-light ">
+                {movie?.release_date || movie?.first_air_date}
+              </p>
+              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
+                HD
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+              <p className="w-5/6">{movie?.overview}</p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres: </span>
+                  {genres?.map((genre) => genre.name).join(', ')}
+                </div>
+                <div>
+                  <span className="text-[gray]">Original Language: </span>
+                  {movie?.original_language}
+                </div>
+                <div>
+                  <span className="text-[gray]">Total Votes: </span>
+                  {movie?.vote_count}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </>
