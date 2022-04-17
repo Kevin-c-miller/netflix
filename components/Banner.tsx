a686558d81f30a44256e0f70fd1fa851459cc97b
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-04-17 11:37:44 -0400

    added a truncate function to shorten the description on the banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -23,6 +23,11 @@ export default function Banner({ netflixOriginals }: Props) {
     )
   }, [netflixOriginals])
 
+  // shortening movie description on the banner
+  const truncate = (str: string, n: number) => {
+    return str?.length > n ? str.substring(0, n - 1) + '...' : str
+  }
+
   return (
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
       <div className=" absolute top-0 left-0 -z-10 h-[95vh] w-screen">
@@ -37,7 +42,8 @@ export default function Banner({ netflixOriginals }: Props) {
         {movie?.name || movie?.title || movie?.original_name}
       </h1>
       <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
-        {movie?.overview}
+        {/* Non-null assertion operator ! */}
+        {truncate(movie?.overview!, 150)}
       </p>
       <div className="flex space-x-3">
         {/* play button on banner */}
