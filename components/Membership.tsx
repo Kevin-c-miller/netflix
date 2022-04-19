c54ff66b0a6e84ad9a76bea612e779d2239e6fdc
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-04-19 18:37:53 -0400

    fixing bug

--- components/Membership.tsx
+++ components/Membership.tsx
@@ -19,7 +19,7 @@ export default function Membership() {
   return (
     <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0">
       <div className="space-y-2 py-4">
-        <h4 className="text-[gray text-lg">Membership & Billing</h4>
+        <h4 className="text-lg text-[gray]">Membership & Billing</h4>
         <button
           disabled={isBillingLoading || !subscription}
           className="h-10 w-3/5 whitespace-nowrap bg-gray-300 py-2 text-sm font-medium text-black shadow-md hover:bg-gray-200 md:w-4/5"
@@ -37,11 +37,11 @@ export default function Membership() {
         <div className="flex flex-col justify-between border-b border-white/10 py-4 md:flex-row">
           <div>
             <p className="font-medium">{user?.email}</p>
-            <p className="text-[gray]">Password: ******</p>
+            <p className="text-[gray]">Password: ********</p>
           </div>
           <div className="md:text-right">
-            <p className="membershipLink">Change Email</p>
-            <p className="membershipLink">Change Password</p>
+            <p className="membershipLink">Change email</p>
+            <p className="membershipLink">Change password</p>
           </div>
         </div>
 
@@ -50,7 +50,7 @@ export default function Membership() {
             <p>
               {subscription?.cancel_at_period_end
                 ? 'Your membership will end on '
-                : 'Your next billing date is '}{' '}
+                : 'Your next billing date is '}
               {subscription?.current_period_end}
             </p>
           </div>
